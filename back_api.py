5a812933bf746ab2ef04cfc9d03841cfac708fa5
Oleg Pavlov <oleg.pavlov@inplatlabs.ru>
2022-03-19 22:40:40 +0300

    some shitcode

--- back_api.py
+++ back_api.py
@@ -77,4 +77,4 @@ class Metals(Resource):
 api.add_resource(Metals, '/metals')  # add endpoints
 
 if __name__ == '__main__':
-    app.run()  # run our Flask app
+    app.run(host='127.0.0.1', port=8000)
