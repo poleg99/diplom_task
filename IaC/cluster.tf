d5c1c50c63c4ba7e6e740b04f3c2cddca732bb23
Oleg Pavlov <oleg.pavlov@inplatlabs.ru>
2022-04-24 10:37:56 +0300

    some code

--- IaC/cluster.tf
+++ IaC/cluster.tf
@@ -23,15 +23,21 @@ module "eks" {
   worker_groups = [
     {
       name                          = "worker-group-1"
-      instance_type                 = "t2.small"
+      instance_type                 = "t2.medium"
+      asg_desired_capacity          = 2
+      additional_security_group_ids = [aws_security_group.worker_group_mgmt_one.id]
+    },
+    {
+      name                          = "worker-group-1"
+      instance_type                 = "t2.large"
       asg_desired_capacity          = 2
       additional_security_group_ids = [aws_security_group.worker_group_mgmt_one.id]
     },
     {
       name                          = "worker-group-2"
-      instance_type                 = "t2.medium"
+      instance_type                 = "t2.large"
       asg_desired_capacity          = 1
       additional_security_group_ids = [aws_security_group.worker_group_mgmt_two.id]
-    },
+    }
   ]
 }
